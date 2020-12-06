c2ebbb513f0498c90f8e20544ae0646189f4d6ea
Michael Spremulli <michaels.wpi@gmail.com>
2020-12-06 17:24:57 -0500

    finance record model

---
+++ FinancialData/src/main/java/com/example/FinancialData/models/FinanceRecord.java
@@ -0,0 +1,95 @@
+package com.example.FinancialData.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class FinanceRecord {
+
+  @Id
+  @GeneratedValue
+  private int id;
+
+  private int step;
+  private  String  type;
+  private double amount;
+  private  String nameOrig;
+  private double oldBalanceOrg;
+  private double newBalanceOrig;
+  private double oldBalanceDest;
+  private double newBalanceDest;
+  private  String nameDest;
+  private int isFraud ;
+
+  public FinanceRecord() {
+  }
+
+  public FinanceRecord(int step,
+                       String type,
+                       double amount,
+                       String nameOrig,
+                       double oldBalanceOrg,
+                       double newBalanceOrig,
+                       double oldBalanceDest,
+                       double newBalanceDest,
+                       String nameDest,
+                       int isFraud) {
+    this.step = step;
+    this.type = type;
+    this.amount = amount;
+    this.nameOrig = nameOrig;
+    this.oldBalanceOrg = oldBalanceOrg;
+    this.newBalanceOrig = newBalanceOrig;
+    this.newBalanceDest = newBalanceDest;
+    this. oldBalanceDest = oldBalanceDest;
+    this.nameDest = nameDest;
+    this.isFraud = isFraud;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public int getStep() {
+    return step;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public String getNameOrig() {
+    return nameOrig;
+  }
+
+  public double getOldBalanceOrg() {
+    return oldBalanceOrg;
+  }
+
+  public double getNewBalanceOrig() {
+    return newBalanceOrig;
+  }
+
+  public double getOldBalanceDest() {
+    return oldBalanceDest;
+  }
+
+  public double getNewBalanceDest() {
+    return newBalanceDest;
+  }
+
+  public String getNameDest() {
+    return nameDest;
+  }
+
+  public int getIsFraud() {
+    return isFraud;
+  }
+}
---
+++ FinancialData/src/main/java/com/example/FinancialData/repositories/financeRepository.java
@@ -0,0 +1,6 @@
+package com.example.FinancialData.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface financeRepository extends JpaRepository<financeRepository, String> {
+}
